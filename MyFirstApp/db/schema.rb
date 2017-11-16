cee017444995a54d2da1f252f6d02a83ce22cca1
Phung Nguyen-Van <phungnv@pc-282.local.elarion.com>
2017-11-16 11:31:47 +0700

    complete step D1 D2

--- MyFirstApp/db/schema.rb
+++ MyFirstApp/db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115035043) do
+ActiveRecord::Schema.define(version: 20171116040804) do
+
+  create_table "carts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
